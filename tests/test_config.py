a46d2cd134f400482321c20d00cb009699338d62
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-10-29 09:11:53 +0200

    fixing tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,31 +1,29 @@
+import os
 import fractalis
 from importlib import reload
-from test.support import EnvironmentVarGuard
 
 class TestConfig:
 
-    env_var_guard = EnvironmentVarGuard()
-
     def test_config_when_test_mode(self):
-        self.env_var_guard.set('FRACTALIS_MODE', 'testing')
+        os.environ['FRACTALIS_MODE'] = 'testing'
         reload(fractalis)
         assert not fractalis.app.config['DEBUG']
         assert fractalis.app.config['TESTING']
 
     def test_config_when_development_mode(self):
-        self.env_var_guard.set('FRACTALIS_MODE', 'development')
+        os.environ['FRACTALIS_MODE'] = 'development'
         reload(fractalis)
         assert fractalis.app.config['DEBUG']
         assert not fractalis.app.config['TESTING']
 
     def test_config_when_production_mode(self):
-        self.env_var_guard.set('FRACTALIS_MODE', 'production')
+        os.environ['FRACTALIS_MODE'] = 'production'
         reload(fractalis)
         assert not fractalis.app.config['DEBUG']
         assert not fractalis.app.config['TESTING']
 
     def test_config_when_default(self):
-        self.env_var_guard.unset('FRACTALIS_MODE')
+        del os.environ['FRACTALIS_MODE']
         reload(fractalis)
         assert not fractalis.app.config['DEBUG']
         assert not fractalis.app.config['TESTING']
