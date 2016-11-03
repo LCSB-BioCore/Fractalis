3cd18e30df007059ce2c3b1bb729c08345030bd8
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-11-03 11:43:36 +0100

    added more test bodies

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -21,6 +21,9 @@ class TestAnalytics(object):
         assert uuid.UUID(response_body['job_id'])
         assert test_client.head(new_resource_url).status_code == 200
 
+    def test_create_new_resource_with_invalid_parameters(self, test_client):
+        assert False
+
     def test_delete_resource(self, test_client):
         response = test_client.post('/analytics')
         response_body = json.loads(response.get_data().decode('utf-8'))
@@ -28,5 +31,17 @@ class TestAnalytics(object):
         assert test_client.delete(new_resource_url).status_code == 200
         assert test_client.head(new_resource_url).status_code == 404
 
-    def test_get_status_new_resource(self, test_client):
-        pass
+    def test_delete_not_existing_resource(self, test_client):
+        assert False
+
+    def test_get_status_running(self, test_client):
+        assert False
+
+    def test_get_status_if_script_failure(self, test_client):
+        assert False
+
+    def test_get_status_if_script_success(self, test_client):
+        assert False
+
+    def test_get_status_for_non_existing_resource(self, test_client):
+        assert False
