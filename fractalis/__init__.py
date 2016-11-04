05ebd104e5291b102014545f2fda1abfd16afb36
Sascha Herzinger <sascha.herzinger@uni.lu>
2016-11-04 15:57:45 +0100

    added test skeleton for sessions

--- fractalis/__init__.py
+++ fractalis/__init__.py
@@ -6,11 +6,15 @@ Modules in this package:
 from flask import Flask
 
 from fractalis.config import configure_app
+from fractalis.session import RedisSessionInterface
 from fractalis.celery import init_celery
 from fractalis.analytics.controllers import analytics
 
+
 flask_app = Flask(__name__)
 configure_app(flask_app)
+flask_app.session_interface = RedisSessionInterface()
+
 celery_app = init_celery(flask_app)
 
 flask_app.register_blueprint(analytics, url_prefix='/analytics')
---
+++ tests/test_session.py
@@ -0,0 +1,21 @@
+import pytest
+
+class TestSession(object):
+
+    def test_add_data_to_session_and_expect_it_in_db(self):
+        assert False
+
+    def test_add_data_and_expect_cookie_set(self):
+        assert False
+
+    def test_dont_add_data_and_exoect_no_cookie_set(self):
+        assert False
+
+    def test_change_session_data_and_expect_change_in_db(self):
+        assert False
+
+    def test_exception_when_manipulating_session_data(self):
+        assert False
+
+    def test_session_data_unavailable_when_expired(self):
+        assert False
