f151014a9cae134b063dcf7564d962cbecee3cd9
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-02-13 13:57:17 +0100

    trying to fix gitlab

--- gitlab-config.py
+++ gitlab-config.py
@@ -1,5 +1,5 @@
 """The only purpose of this file is to have a running CI test environment."""
-REDIS_HOST='redis'
+REDIS_HOST = 'redis'
 
-BROKER_URL='amqp://guest:guest@rabbitmq:5672//'
-CELERY_RESULT_BACKEND='redis://redis:6379'
+BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
