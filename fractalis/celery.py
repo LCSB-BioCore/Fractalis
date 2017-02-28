c5dc4242e55671d0aa93f955656e7873d3d06d31
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-02-28 10:12:21 +0100

    fixing issue when couldn't set conf via env var

--- fractalis/celery.py
+++ fractalis/celery.py
@@ -1,12 +1,12 @@
 """This module is responsible for the establishment and configuration of a
 Celery instance."""
 import os
-import sys
 import logging
 
 from celery import Celery
 
 from fractalis.utils import list_classes_with_base_class
+from fractalis.utils import import_module_by_abs_path
 from fractalis.data.etls.etl import ETL
 from fractalis.analytics.job import AnalyticsJob
 
@@ -14,13 +14,10 @@ from fractalis.analytics.job import AnalyticsJob
 app = Celery(__name__)
 app.config_from_object('fractalis.config')
 try:
-    config_file = os.environ['FRACTALIS_CONFIG']
-    module = os.path.splitext(os.path.basename(config_file))[0]
-    sys.path.append(os.path.dirname(os.path.expanduser(config_file)))
-    config = __import__(module).__dict__
+    module = import_module_by_abs_path(os.environ['FRACTALIS_CONFIG'])
     for key in app.conf:
-        if key in config and key.startswith('_'):
-            app.conf[key] = config[key]
+        if key in module.__dict__ and not key.startswith('_'):
+            app.conf[key] = module.__dict__[key]
 except KeyError:
     logger = logging.getLogger('fractalis')
     logger.warning("FRACTALIS_CONFIG is not set. Using defaults.")
