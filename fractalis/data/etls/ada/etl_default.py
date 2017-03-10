3561fb1bf021d18cbd6d9aed061dfa00028b257b
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-03-10 11:18:01 +0100

    Including ID into every ADA ETL

--- fractalis/data/etls/ada/etl_default.py
+++ fractalis/data/etls/ada/etl_default.py
@@ -16,19 +16,29 @@ class DefaultETL(ETL):
     def extract(self, server: str,
                 token: str, descriptor: dict) -> object:
         data_set = descriptor['data_set']
-        projection = descriptor['projection']
+        projections = ['_id']  # we always ask for an id
+        projections += [descriptor['projection']]
         split = token.split('=')
         assert len(split) == 2
         cookie = {split[0]: split[1][1:-1]}
         r = requests.get(url='{}/studies/records/findCustom'.format(server),
                          headers={'Accept': 'application/json'},
-                         params={'dataSet': data_set, 'projection': projection},
+                         params={
+                             'dataSet': data_set,
+                             'projection': projections
+                         },
                          cookies=cookie)
         if r.status_code != 200:
             raise ValueError("Data extraction failed. Reason: [{}]: {}"
                              .format(r.status_code, r.text))
         return r.json()
 
-    def transform(self, raw_data: object) -> DataFrame:
-        df = DataFrame(raw_data)
+    def transform(self, raw_data) -> DataFrame:
+        data = []
+        for row in raw_data:
+            id = row['_id']['$oid']
+            del row['_id']
+            row['id'] = id
+            data.append(row)
+        df = DataFrame(data)
         return df
