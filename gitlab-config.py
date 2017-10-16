466db4550ddcdf0471f0e32822924f7286d1a75b
sascha.herzinger <sascha.herzinger@uni.lu>
2017-10-16 14:43:16 +0200

    brand new gitlab ci file to fix runner

--- gitlab-config.py
+++
@@ -1,6 +0,0 @@
-"""The only purpose of this file is to have a running CI test environment."""
-
-REDIS_HOST = 'redis'
-
-BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
