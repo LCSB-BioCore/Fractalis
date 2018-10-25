0f034fa178774084382e1d322ac0f323f8180a90
sherzinger <sascha.herzinger@uni.lu>
2018-10-25 13:43:24 +0200

    Bump version: 1.2.3 → 1.3.0

--- fractalis/misc/controller.py
+++ fractalis/misc/controller.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 @misc_blueprint.route('/version', methods=['GET'])
 def get_version() -> Tuple[Response, int]:
-    version = '1.2.3'
+    version = '1.3.0'
     # this is a good place to launch the janitor because /version is one of the
     # first requests sent by the front-end on initialization
     janitor.delay()
